5396f543cefc4586cbf2464ef7cfd29d14a6019c
Michael Hoffert <mchoffert14@gmail.com>
2025-07-16 21:45:50 +0000

    setup.py and testing code draft

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="phylo_blocks",
+    version="0.1.0",
+    description="Phylogenetic blocking with ete3",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Michael Hoffert",
+    author_email="mchoffert14@gmail.com",
+)
